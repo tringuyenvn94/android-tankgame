f196eeb92c25a771de71cd579fea765e0b7df09d
trinhvanbinh.hp@gmail.com <trinhvanbinh.hp@gmail.com@1c093e2e-9d54-46b4-3a92-0c1e945b9df8>
2012-06-28 10:33:26 +0000

    Em làm cái giao diện menu rồi đấy. Phần xét sự lắng nghe nút highscore hình như có vấn đề mọi người sửa giúp em với ạ. Mà sao lúc em load games bị lỗi nhỉ @@

--- trunk/ProjectCode/TankGame/src/NIIT/Android3/TankGame/Interface/MenuActivity.java
+++ trunk/ProjectCode/TankGame/src/NIIT/Android3/TankGame/Interface/MenuActivity.java
@@ -1,39 +1,59 @@
 package NIIT.Android3.TankGame.Interface;
 
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+
 import NIIT.Android3.TankGame.R;
 import NIIT.Android3.TankGame.R.id;
 import NIIT.Android3.TankGame.R.layout;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MenuActivity extends Activity {
-	
+
 	private ImageView imageMenuNewGame;
 	private ImageView imageMenuHighScore;
 	private ImageView imageMenuOptions;
 	private ImageView imageMenuAboutUs;
 	private ImageView imageMenuHelp;
 	private ImageView imageMenuExit;
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        
-        setContentView(R.layout.menu);
-        
-        imageMenuNewGame = (ImageView)findViewById(R.id.menu_newgame);
-        imageMenuHighScore = (ImageView)findViewById(R.id.menu_highscore);
-        imageMenuOptions = (ImageView)findViewById(R.id.menu_options);
-        imageMenuAboutUs = (ImageView)findViewById(R.id.menu_aboutus);
-        imageMenuHelp = (ImageView)findViewById(R.id.menu_help);
-        imageMenuExit = (ImageView)findViewById(R.id.menu_exit);
-        
-    }
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		setContentView(R.layout.menu);
+
+		imageMenuNewGame = (ImageView) findViewById(R.id.menu_newgame);
+		imageMenuHighScore = (ImageView) findViewById(R.id.menu_highscore);
+		Button highButton = (Button) findViewById(R.id.menu_highscore);
+		highButton.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				setContentView(R.layout.highscore);
+
+			}
+		});
+
+		imageMenuOptions = (ImageView) findViewById(R.id.menu_options);
+		imageMenuAboutUs = (ImageView) findViewById(R.id.menu_aboutus);
+		imageMenuHelp = (ImageView) findViewById(R.id.menu_help);
+		imageMenuExit = (ImageView) findViewById(R.id.menu_exit);
+
+	}
 }
