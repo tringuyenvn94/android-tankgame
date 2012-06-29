2d792d1f0e8050db75585145df43fd2883f6d85a
trinhvanbinh.hp <trinhvanbinh.hp@gmail.com>
2012-06-29 22:15:46 +0000

    cai exit de em lam r :))

--- ProjectCode/TankGame/gen/NIIT/Android3/TankGame/R.java
+++ ProjectCode/TankGame/gen/NIIT/Android3/TankGame/R.java
@@ -22,13 +22,14 @@ public final class R {
         public static final int splash=0x7f020008;
     }
     public static final class id {
-        public static final int menu_aboutus=0x7f060004;
-        public static final int menu_exit=0x7f060005;
-        public static final int menu_help=0x7f060003;
-        public static final int menu_highscore=0x7f060001;
-        public static final int menu_newgame=0x7f060000;
-        public static final int menu_options=0x7f060002;
-        public static final int splash_Img=0x7f060006;
+        public static final int back=0x7f060000;
+        public static final int menu_aboutus=0x7f060005;
+        public static final int menu_exit=0x7f060006;
+        public static final int menu_help=0x7f060004;
+        public static final int menu_highscore=0x7f060002;
+        public static final int menu_newgame=0x7f060001;
+        public static final int menu_options=0x7f060003;
+        public static final int splash_Img=0x7f060007;
     }
     public static final class layout {
         public static final int highscore=0x7f030000;
